abae1799b1be46c5919f1bf70f51b294995cdc6c
OmerZeliger <omer.zeliger@gmail.com>
2021-11-12 14:15:21 -0500

    Added a sprite and a player control script

---
+++ Assets/MovementController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    // Awake is called before start, when script is being loaded
+    void Awake()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
