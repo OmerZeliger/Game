6f12b3b8ada7e0a2bab22b6d854f6c3ec187a157
OmerZeliger <omer.zeliger@gmail.com>
2022-03-30 22:17:18 -0400

    messed around more with animation

---
+++ Assets/Scripts/CaterpillarMotionTest.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaterpillarMotionTest : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(-0.6f, 0);
+    }
+}
